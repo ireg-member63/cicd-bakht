28a53252f4e260ec5a04a63c34716f0b1e36b737
ireg-member63 <ireg.member63@gmail.com>
2025-05-08 10:15:15 +0500

    Version 2 - updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Hello from Version 1 🚀</h1>
-      <p>This is the first version of our sample React app.</p>
+      <h1>Hello from Version 2 🎉</h1>
+      <p>We have successfully updated the app using CI/CD!</p>
     </div>
   );
 }
